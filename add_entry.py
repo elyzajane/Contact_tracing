863918311cf27cfc7e719f500651925a3a8c52df
Elyza <elyzajanerelucio@gmail.com>
2023-07-27 12:15:37 +0800

    Create a title label and frame

---
+++ add_entry.py
@@ -0,0 +1,17 @@
+import tkinter as tk
+from tkinter import messagebox
+
+class ContactTracingApp(tk.Tk):
+    def __init__(self):
+        super().__init__()
+        self.title("Covid-19 Contact Tracing Form")
+        self.geometry("1000x800")
+
+        title_frame = tk.Frame(self, bg="#4CAF50")
+        title_frame.pack(fill="x")
+        self.title_label = tk.Label(title_frame, text="Covid-19 Contact Tracing Form", font=("Helvetica", 20, "bold"), fg="white", bg="#4CAF50")
+        self.title_label.pack(pady=10)
+
+        self.message_frame = tk.Frame(self, bg="#f0f0f0")
+        main_message_text = "Trace any possible contact a person may have had with anyone who could be a carrier of the COVID-19 virus. The Contact Tracing survey will help track down people who are in danger of being exposed to the virus. This can lead to timely detection and treatment, as well as preventing it from spreading further. Use the Contact Tracing form within your organization, community, clients, and more to detect possible exposure to the virus."
+
