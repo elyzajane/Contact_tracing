9bec35ce33812094ad71c6b0ea9ca928ce222c90
Elyza <elyzajanerelucio@gmail.com>
2023-07-28 01:43:59 +0800

    Create a title

--- main.py
+++ main.py
@@ -2,3 +2,11 @@ import tkinter as tk
 from search_entry import SearchEntry
 from add_entry import AddEntry
 
+class Main(tk.Tk):
+    
+    def __init__(self):
+        super().__init__()
+
+        self.title("Contact Tracing App")
+        self.geometry("700x500")
+        self.pack_propagate(0)
