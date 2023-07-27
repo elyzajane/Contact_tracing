6091ee0bf4e6f9f52aa69ab3607daca6738372ea
Elyza <elyzajanerelucio@gmail.com>
2023-07-28 01:48:50 +0800

    Add entry and search button

--- main.py
+++ main.py
@@ -10,3 +10,12 @@ class Main(tk.Tk):
         self.title("Contact Tracing App")
         self.geometry("700x500")
         self.pack_propagate(0)
+
+        AddEntryButton = tk.Button(self, text="Add Entry", command=self.open_add_entry)
+        AddEntryButton.place(x=320, y=280)
+
+        SearchEntryButton = tk.Button(self, text="Search Entry", command=self.open_search_entry)
+        SearchEntryButton.place(x=313, y=310)
+        
+        close = tk.Button(self, text="Close", command=self.close_window)
+        close.place(x=335, y=450)
